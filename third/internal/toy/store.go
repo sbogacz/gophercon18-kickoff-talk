c075038ef1b2a8cc8c17b91c20578d57e5bf0676
sbogacz <steven.bogacz@gmail.com>
2018-08-26 21:58:32 -0600

    add in accidentally ignored files

---
+++ third/internal/toy/store.go
@@ -0,0 +1,11 @@
+package toy
+
+import "context"
+
+// Store provides an interface to the blobs we
+// store in the toy app
+type Store interface {
+	Get(context.Context, string) (string, error)
+	Set(context.Context, string, string) error
+	Del(context.Context, string) error
+}
